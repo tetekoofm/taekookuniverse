b908cd70e242c783dd4e732effa5bff7df26764d
tetekoofm <tetekoofm@gmail.com>
2025-11-29 21:17:09 -0600

    tk cooking game updates

--- static/js/cook_with_taekook.js
+++ static/js/cook_with_taekook.js
@@ -3,6 +3,7 @@ const landingScene = document.getElementById('landingScene');
 const rideScene = document.getElementById('rideScene');
 const restaurantScene = document.getElementById('restaurantScene');
 const motorbike = document.getElementById('motorbike');
+const sky = document.getElementById('sky');
 
 startBtn.addEventListener('click', () => {
     // Hide landing, show ride scene
@@ -12,7 +13,7 @@ startBtn.addEventListener('click', () => {
     // Animate motorbike across screen
     let pos = -300;
     const interval = setInterval(() => {
-        pos += 5; // speed
+        pos += 7; // speed
         motorbike.style.right = pos + 'px';
         if(pos > window.innerWidth) {
             clearInterval(interval);
@@ -22,3 +23,16 @@ startBtn.addEventListener('click', () => {
         }
     }, 30);
 });
+
+for (let i = 0; i < 100; i++) {
+    const star = document.createElement('div');
+    star.classList.add('star');
+    star.style.top = Math.random() * 80 + "%";   // keep stars above road
+    star.style.left = Math.random() * 100 + "%";
+    star.style.width = (Math.random() * 2 + 1) + "px";
+    star.style.height = star.style.width;
+    star.style.background = Math.random() > 0.5 ? "#8affc1" : "#c88aff";
+    sky.appendChild(star);
+}
+
+
