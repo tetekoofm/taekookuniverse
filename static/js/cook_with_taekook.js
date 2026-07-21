f6aba352410f08783e8a9d113bc7b0f954bd726b
tetekoofm <tetekoofm@gmail.com>
2026-07-20 21:35:35 -0500

    cook with tk changes clean up

--- static/js/cook_with_taekook.js
+++ static/js/cook_with_taekook.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded",()=>{
        TEST MODE
     -------------------------------------------*/
     const TEST_MODE=false; // change to true for direct scene testing
-    const TEST_SCENE="delivery"; // landing, ride, restaurant, kitchen, ingredient, cooking, delivery, receipt
+    const TEST_SCENE="restaurant"; // landing, ride, restaurant, kitchen, ingredient, cooking, delivery, receipt
     const TEST_RECIPE=
     "Strawberry Milkshake";
     // "Banana Milkshake";
